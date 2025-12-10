24d330fd4441476e1db7436096112f76a17573d2
Edwin Sutanudjaja <edwinkost@heol>
2025-12-10 15:56:45 +0100

    re-run irrigation downscaling - fixing output file names

--- scripts/run_irrigation_downscaling.sh
+++ scripts/run_irrigation_downscaling.sh
@@ -1,6 +1,10 @@
 
-# ~ python irrigation_downscaling.py /scratch-shared/edwin/irrigation_downscaling/rhine_30sec/ssp5_2015_2100/ /scratch-shared/edwin/irrigated_area_jen_05min/irrigated_area_05min_hectar_meier_g_aei_ssp5_2015_2100.nc 
+python irrigation_downscaling.py /scratch-shared/edwin/irrigation_downscaling/rhine_30sec/ssp5_2015_2100/ /scratch-shared/edwin/irrigated_area_jen_05min/irrigated_area_05min_hectar_meier_g_aei_ssp5_2015_2100.nc irrigated_area_30sec_hectar_meier_g_aei_ssp5_2015_2100_v20250310.nc &
 
 python irrigation_downscaling.py /scratch-shared/edwin/irrigation_downscaling/rhine_30sec/ssp3_2015_2100/ /scratch-shared/edwin/irrigated_area_jen_05min/irrigated_area_05min_hectar_meier_g_aei_ssp3_2015_2100.nc 
+irrigated_area_30sec_hectar_meier_g_aei_ssp3_2015_2100_v20250310.nc &
 
 python irrigation_downscaling.py /scratch-shared/edwin/irrigation_downscaling/rhine_30sec/ssp1_2015_2100/ /scratch-shared/edwin/irrigated_area_jen_05min/irrigated_area_05min_hectar_meier_g_aei_ssp1_2015_2100.nc 
+irrigated_area_30sec_hectar_meier_g_aei_ssp1_2015_2100_v20250310.nc &
+
+wait
